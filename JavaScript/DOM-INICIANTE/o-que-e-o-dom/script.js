227adb6f81f209f2973f45893e0af83de833a1a2
pescador98 <isacoliveira1511@gmail.com>
2024-10-06 11:57:28 -0300

    JavaScript estudo

---
+++ JavaScript/DOM-INICIANTE/o-que-e-o-dom/script.js
@@ -0,0 +1,13 @@
+// Retorne o url da página atual utilizando o objeto window
+const url = document.URL;
+console.log(url);
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const elementoAtivo = document.querySelector(".ativo");
+console.log(elementoAtivo);
+// Retorne a linguagem do navegador
+const lang = document.documentElement.lang;
+console.log(lang);
+// Retorne a largura da janela
+const width = document.documentElement.clientWidth;
+console.log(width);
