135c01cd83bdc1777f230e60c3cd7dd3f5b2cc14
pescador98 <isacoliveira1511@gmail.com>
2024-10-10 16:54:34 -0300

    subindo estudos

---
+++ JavaScript/DOM-INICIANTE/selecionar-o-dom - Copia/script-aula.js
@@ -0,0 +1,5 @@
+const gridSectionHTML = document.getElementsByClassName("grid-section");
+const gridSectionNode = document.querySelectorAll(".grid-section");
+
+console.log(gridSectionHTML);
+console.log(gridSectionNode);
